8db98b8a8d525a556e96b06b83932008b7dcedcb
daus-s <dauscarmichael@gmail.com>
2023-02-21 00:44:15 -0800

    added routing to app

--- src/App.js
+++ src/App.js
@@ -7,16 +7,23 @@ import "./linklist.css";
 import "./projectcard.css";
 import "./spacer.css";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./Header";
 import CardList from "./CardList";
 import LinkList from "./LinkList";
 
 export default function App() {
   return (
-    <div className="App">
-      <Header />
-      <LinkList />
-      <CardList />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <LinkList />
+        <CardList />
+      </div>
+      <Routes>
+        <Route exact path="/" element={<App />} />
+        <Route path="/tictactoe" element={<TTT />} />
+      </Routes>
+    </Router>
   );
 }
