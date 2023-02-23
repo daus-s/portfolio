52ef73e7c51c4f7646c6259c855ffe1e5b94e017
daus-s <dauscarmichael@gmail.com>
2023-02-23 10:42:32 -0800

    styles.css changed to have new styling on replaced class app. replaced app with home. refactored app to become navigation

--- src/App.js
+++ src/App.js
@@ -8,21 +8,15 @@ import "./projectcard.css";
 import "./spacer.css";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./Header";
-import CardList from "./CardList";
-import LinkList from "./LinkList";
+import Home from "./Home.js";
+import TTT from "./TTT.js";
 
 export default function App() {
   return (
     <Router>
-      <div className="App">
-        <Header />
-        <LinkList />
-        <CardList />
-      </div>
       <Routes>
-        <Route exact path="/" element={<App />} />
-        <Route path="/tictactoe" element={<TTT />} />
+        <Route exact path="/" element={Home} />
+        <Route path="/tictactoe" element={TTT} />
       </Routes>
     </Router>
   );
