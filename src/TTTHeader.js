37e866d793e72c40a2d022687b31b188b0e918e3
daus-s <dauscarmichael@gmail.com>
2023-07-27 10:54:30 -0700

    changed title to tttTitle to avoid overwriting rules

--- src/TTTHeader.js
+++ src/TTTHeader.js
@@ -8,7 +8,7 @@ export default function TTTHeader(props) {
     : {};
   const isMobile = useMediaQuery("(max-width:592px)");
   return (
-    <div className="title">
+    <div className="tttTitle">
       {isMobile ? <h2>Tic-Tac-Toe</h2> : <h1>Tic-Tac-Toe</h1>}
       <InfoButton handle={props.openInfo} style={mobileStyle} />
     </div>
