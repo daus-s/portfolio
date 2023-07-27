f61c7fa2aaca6d3e31d46b98d7ffc445835ff104
daus-s <dauscarmichael@gmail.com>
2023-07-27 07:18:13 -0700

    missing object

---
+++ src/InfoButton.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import "./styles.css";
+
+export default function InfoButton(props) {
+  const originalSrc =
+    "https://github.com/daus-s/portfolio/blob/main/public/info-gray.png?raw=true";
+  const hoverSrc =
+    "https://github.com/daus-s/portfolio/blob/main/public/info-green.png?raw=true";
+
+  useEffect(() => {
+    const imageElement = document.getElementById("infobutton");
+    if (imageElement) {
+      imageElement.addEventListener("mouseover", () => {
+        imageElement.src = hoverSrc;
+      });
+
+      imageElement.addEventListener("mouseout", () => {
+        imageElement.src = originalSrc;
+      });
+    }
+  }, [originalSrc, hoverSrc]);
+
+  return (
+    <button className="infobutton" onClick={props.handle}>
+      <img
+        src={originalSrc}
+        id="infobutton"
+        alt="Icon to reset the board"
+        className="icon"
+      />
+    </button>
+  );
+}
