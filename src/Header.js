7c59f7d07dfd641ee4684d04f1f3d3d92281c2ce
daus-s <dauscarmichael@gmail.com>
2023-08-10 11:10:40 -0700

    changed hover functionality on title to toggling when entering and being persistent until mouse enters again

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,7 @@
 import "./styles.css";
 import "./app.css";
-import "./cardlist.css";
-import "./data.css";
+
 import "./header.css";
-import "./linklist.css";
-import "./projectcard.css";
-import "./spacer.css";
 
 import React from "react";
 import { useMediaQuery } from "@material-ui/core";
@@ -13,30 +9,25 @@ import { useMediaQuery } from "@material-ui/core";
 const Header = () => {
   const isMobile = useMediaQuery("(max-width:600px)");
   const [text, setText] = React.useState("Davis Carmichael's Portfolio");
+  const [ver, setVer] = React.useState(true);
 
   const onMouseover = () => {
-    setText("Daus Carmichael's Portfolio");
-  };
-  const onMouseout = () => {
-    setText("Davis Carmichael's Portfolio");
+    setVer(!ver);
+    if (!ver) {
+      setText("Davis Carmichael's Portfolio");
+    } else if (ver) {
+      setText("Daus Carmichael's Portfolio");
+    }
   };
 
   return (
     <>
       {isMobile ? (
-        <h3
-          className="Header"
-          onMouseEnter={onMouseover}
-          onMouseLeave={onMouseout}
-        >
+        <h3 className="Header" onMouseEnter={onMouseover}>
           {text}
         </h3>
       ) : (
-        <h1
-          className="Header"
-          onMouseEnter={onMouseover}
-          onMouseLeave={onMouseout}
-        >
+        <h1 className="Header" onMouseEnter={onMouseover}>
           {text}
         </h1>
       )}
